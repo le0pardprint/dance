ce033cdd6b003abe5125fb3dfd160c4780b44cf7
le0pardprint <tuflevpetr@gmail.com>
2026-05-13 23:34:04 +0300

    исправлен Program.cs: добавлен ReferenceHandler.IgnoreCycles для устранения циклических ссылок в JSON

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
-﻿using dance.API.Data;  // ← ДОБАВИТЬ (для подключения AppDbContext)
-using Microsoft.EntityFrameworkCore;  // ← ДОБАВИТЬ (для UseSqlServer)
+﻿using dance.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;  // ← ДОБАВИТЬ ЭТО
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// === ДОБАВЬТЕ ЭТОТ БЛОК ===
+// Регистрация DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// ========================
+
+// CORS для фронтенда
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -25,6 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReact");
 
 app.UseAuthorization();
 
