703f2f484c00b10b3b448d32ebc4d8b6bc06f699
le0pardprint <tuflevpetr@gmail.com>
2026-05-17 23:12:15 +0300

    исправлен docker healthcheck и restart policy

---
+++ Models/Directions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dance.API.Models
+{
+    public class Direction
+    {
+        [Key]
+        public int Direction_id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AgeGroup { get; set; }
+        public string Level { get; set; }
+
+        public ICollection<Group>? Groups { get; set; }
+    }
+}
---
+++ Models/Trainers.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dance.API.Models
+{
+    public class Trainer
+    {
+        [Key]
+        public int Trainer_id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Direction_id { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // Связь с User
+        public int? User_id { get; set; }
+        public User? User { get; set; }
+
+        // Навигационные свойства
+        public Direction? Direction { get; set; }
+        public ICollection<Group>? Groups { get; set; }
+        public ICollection<Class>? Classes { get; set; }
+    }
+}
